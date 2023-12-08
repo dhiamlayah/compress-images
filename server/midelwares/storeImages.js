87d4f1a598ada649a064a6450a9bad5960c3b103
dhiamlayah <113603828+dhiamlayah@users.noreply.github.com>
2023-12-08 01:44:56 +0000

    store the image in static file and resize it

---
+++ server/midelwares/storeImages.js
@@ -0,0 +1,34 @@
+const axios = require('axios')
+const fs = require('fs')
+const path = require('path');
+
+const asyncMiddleware = (fn) => (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+  
+async function storeImage (req,res,next){
+
+    const imageUrl = req.query.url;
+    try{
+        
+        const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+
+        const filename = `downloaded_image_${Date.now()}.jpg`;
+
+        // Define the path where the image will be saved
+        const filePath = path.join(__dirname, '../Picture', filename);
+
+        // Save the image to the server
+        await fs.writeFileSync(filePath, response.data);
+    
+        req.fileName=filename ,
+        next()
+        
+    }catch(err){
+        console.log('we get an error here',err)
+    }
+
+
+}
+
+module.exports=  storeImage
--- server/server.js
+++ server/server.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const path = require('path');
+const resizeImages = require('./midelwares/resizeImage')
+const storeImage =require('./midelwares/storeImages') 
+app.use('/Picture', express.static('Picture'));
+app.use('/userPicture', express.static('userPicture'));
 
 
+app.get('/image',storeImage,resizeImages,async (req,res)=>{
+    fileName = req.fileName
+    const imagePath = path.join(__dirname, '../Picture', fileName);
+
+    console.log('fileName',imagePath)
+    res.send("successfuly")
+
+})
 
 app.listen(port,()=>{
     console.log(`Server is listening at http://localhost:${port}`)
